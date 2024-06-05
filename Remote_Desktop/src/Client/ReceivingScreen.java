c6696046b444d75dc8dff0991ee033c7a8dbcbe4
Ryuma <quangtruc6a1vx@gmail.com>
2024-06-05 18:54:15 +0700

    optimize ReceivingScreen

--- Remote_Desktop/src/Client/ReceivingScreen.java
+++ Remote_Desktop/src/Client/ReceivingScreen.java
@@ -3,12 +3,13 @@ package Client;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 
 public class ReceivingScreen extends Thread {
     private final DataInputStream dis; // to get the size of the screenshot image
     private JPanel cPanel = null;
-    Image serverScreen = null;
+    BufferedImage serverScreen = null;
 
     public ReceivingScreen(InputStream in, JPanel cPanel) {
         this.dis = new DataInputStream(new BufferedInputStream(in));
@@ -19,6 +20,7 @@ public class ReceivingScreen extends Thread {
     public void run() {
         try{
             while(true) {
+                Image scaledImg = null;
                 // compute the size of the image
                 int length = dis.readInt();
                 byte[] imageBytes = new byte[length];
@@ -28,12 +30,12 @@ public class ReceivingScreen extends Thread {
                 serverScreen = ImageIO.read(new ByteArrayInputStream(imageBytes));
 
                 if (serverScreen != null) {
-                    serverScreen = serverScreen.getScaledInstance(cPanel.getWidth(), cPanel.getHeight(), Image.SCALE_SMOOTH);
+                    scaledImg = serverScreen.getScaledInstance(cPanel.getWidth(), cPanel.getHeight(), Image.SCALE_SMOOTH);
                 }
 
                 // add the image to cPanel
                 Graphics graphics = cPanel.getGraphics();
-                graphics.drawImage(serverScreen, 0, 0, cPanel.getWidth(), cPanel.getHeight(), cPanel);
+                graphics.drawImage(scaledImg, 0, 0, cPanel.getWidth(), cPanel.getHeight(), cPanel);
             }
         }catch (IOException e) {
             e.printStackTrace();
